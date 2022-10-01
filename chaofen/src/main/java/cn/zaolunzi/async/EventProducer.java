d1ab7340dad48ef175298548208a2c3a28315196
selectbook <18702515445@163.com>
2022-10-02 00:27:52 +0800

    add login controller

---
+++ chaofen/src/main/java/cn/zaolunzi/async/EventProducer.java
@@ -0,0 +1,24 @@
+package cn.zaolunzi.async;
+
+import cn.zaolunzi.utils.JedisAdapter;
+import cn.zaolunzi.utils.RedisKeyUtil;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventProducer {
+    @Autowired
+    JedisAdapter jedisAdapter;
+
+    public boolean fireEvent(EventModel eventModel) {
+        try {
+            String json = JSONObject.toJSONString(eventModel);
+            String key = RedisKeyUtil.getEventQueueKey();
+            jedisAdapter.lpush(key, json);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
